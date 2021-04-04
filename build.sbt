58e79f7e181794664082bfb5de948e09fe4fa8f5
Facundo Bocalandro <facundo@bocalandro.com.ar>
2021-04-04 19:21:44 -0300

    Initial commit

---
+++ build.sbt
@@ -0,0 +1,29 @@
+name := "geo-service"
+
+version := "0.1"
+
+scalaVersion := "2.13.5"
+
+val circeVersion = "0.12.3"
+
+libraryDependencies ++= Seq(
+  "io.circe" %% "circe-core",
+  "io.circe" %% "circe-generic",
+  "io.circe" %% "circe-parser"
+).map(_ % circeVersion)
+
+libraryDependencies ++= Seq(
+  "io.grpc" % "grpc-netty" % scalapb.compiler.Version.grpcJavaVersion,
+  "com.thesamet.scalapb" %% "scalapb-runtime-grpc" % scalapb.compiler.Version.scalapbVersion
+)
+
+Compile / PB.targets := Seq(
+  scalapb.gen() -> (Compile / sourceManaged).value / "scalapb"
+)
+
+
+libraryDependencies ++= Seq(
+  "com.thesamet.scalapb" %% "scalapb-runtime" % scalapb.compiler.Version.scalapbVersion % "protobuf"
+)
+
+libraryDependencies += "com.thesamet.scalapb" %% "scalapb-json4s" % "0.11.0"
---
+++ src/main/scala/GeoService.scala
@@ -0,0 +1,140 @@
+import geo_service.GeoServiceGrpc.{GeoService, GeoServiceStub}
+import geo_service.{City, Country, GeoServiceGrpc, GetCitiesByProvinceReply, GetCitiesByProvinceRequest, GetCountriesListReply, GetCountriesListRequest, GetCountryAndProvinceByIPReply, GetCountryAndProvinceByIPRequest, GetProvincesByCountryReply, GetProvincesByCountryRequest, Province}
+import io.grpc.stub.StreamObserver
+import io.grpc.{ManagedChannelBuilder, ServerBuilder}
+
+import scala.io._
+import scala.concurrent.ExecutionContext.global
+import scala.concurrent.{ExecutionContext, ExecutionContextExecutor, Future}
+
+class MyService extends GeoService {
+  val locationDatabase = new CSVReader()
+
+  override def countriesList(request: GetCountriesListRequest): Future[GetCountriesListReply] = {
+    val reply = GetCountriesListReply(countries = locationDatabase.getCountries)
+    Future.successful(reply)
+  }
+
+  override def provincesByCountry(request: GetProvincesByCountryRequest): Future[GetProvincesByCountryReply] = {
+    val reply = GetProvincesByCountryReply(provinces = locationDatabase.getProvincesByCountry(request.country.orNull))
+    Future.successful(reply)
+  }
+
+  override def citiesByProvince(request: GetCitiesByProvinceRequest): Future[GetCitiesByProvinceReply] = {
+    val reply = GetCitiesByProvinceReply(cities = locationDatabase.getCitiesByProvince(request.province.orNull))
+    Future.successful(reply)
+  }
+
+  override def countryAndProvinceByIP(request: GetCountryAndProvinceByIPRequest): Future[GetCountryAndProvinceByIPReply] = {
+    val replyTuple = locationDatabase.getCountryAndProvinceByIP(request.ip)
+    val reply = GetCountryAndProvinceByIPReply(country = Option(replyTuple._1), province = Option(replyTuple._2))
+    Future.successful(reply)
+  }
+}
+
+trait LocationDatabase {
+  def getCountries: List[Country]
+
+  def getProvincesByCountry(country: Country): List[Province]
+
+  def getCitiesByProvince(province: Province): List[City]
+
+  def getCountryAndProvinceByIP(ip: String): (Country, Province)
+}
+
+class CSVReader extends LocationDatabase {
+
+  import CSVReader._
+  import io.circe._
+  import io.circe.parser._
+
+  val data: List[WorldCity] = getWorldCities("src/main/world-cities.csv")
+
+  def getCountries: List[Country] = {
+    data.map(_.country)
+  }
+
+  def getProvincesByCountry(country: Country): List[Province] = {
+    data.filter(_.country == country).map(_.province).distinct
+  }
+
+  def getCitiesByProvince(province: Province): List[City] = {
+    data.filter(_.province == province).map(_.city).distinct
+  }
+
+  def getCountryAndProvinceByIP(ip: String): (Country, Province) = {
+    val source = Source.fromURL("https://ipapi.co/" + ip + "/json/")
+    val content: Json = parse(source.mkString).getOrElse(null)
+    source.close()
+    val countryString: String = content.\\("country_name").head.asString.getOrElse("")
+    val provinceString: String = content.\\("region").head.asString.getOrElse("")
+    val country: Country = Country(name = countryString)
+    (country, Province(name = provinceString, Option(country)))
+  }
+}
+
+case class IPResponseObject(country: Country, province: Province)
+
+case class WorldCity(city: City, country: Country, province: Province, geoNameId: Long)
+
+object CSVReader {
+
+  def getWorldCities(filePath: String): List[WorldCity] = {
+    val fileSource = Source.fromFile(filePath)
+    val data: List[List[String]] = fileSource.getLines().toList.map(_.split(',').toList)
+    fileSource.close()
+    data.map {
+      case List(cityString, countryString, provinceString, geoNameId) =>
+        val country: Country = Country(name = countryString)
+        val province: Province = Province(name = provinceString, country = Option(country))
+        val city: City = City(name = cityString, province = Option(province))
+        WorldCity(city, country, province, geoNameId.toLong)
+    }
+  }
+
+}
+
+object GeoServiceServer extends App {
+  val builder = ServerBuilder.forPort(50000)
+
+  builder.addService(
+    GeoService.bindService(new MyService(), ExecutionContext.global)
+  )
+
+  val server = builder.build()
+  server.start()
+
+  println("Running....")
+  server.awaitTermination()
+}
+
+object ClientDemo extends App {
+
+  implicit val ec: ExecutionContextExecutor = ExecutionContext.global
+
+
+  def createStub(ip: String, port: Int = 50000): GeoServiceStub = {
+    val builder = ManagedChannelBuilder.forAddress(ip, port)
+    builder.usePlaintext()
+    val channel = builder.build()
+
+    GeoServiceGrpc.stub(channel)
+  }
+
+  val stub1 = createStub("127.0.0.1", 50000)
+  val stub2 = createStub("127.0.0.1", 50001)
+
+  val stubs = List(stub1, stub2)
+  val healthyStubs = stubs
+
+
+  // Say hello (request/response)
+  val response: Future[GetCountriesListReply] = stub1.countriesList(GetCountriesListRequest())
+
+  response.onComplete { r =>
+    println("Response: " + r)
+  }
+
+
+  System.in.read()
+}
